a05d136b73e072c3469d4550744a9896ad7ca43d
rasathelamedude <rasyar.safen19@gmail.com>
2025-09-17 11:16:01 +0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp as Widget);
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
